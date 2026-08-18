461f3422b58b42338282f3c9591d413eb35a2d38
K4kurtis <krishan15061998@gmail.com>
2026-08-18 19:34:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,9 @@
    CONFIGURATION
 ========================================================= */
 
-const SHOP_LATITUDE = "27.851632";
+const SHOP_LATITUDE = "27.851625";
 
-const SHOP_LONGITUDE = "75.270942";
+const SHOP_LONGITUDE = "75.270945";
 
 const SHOPKEEPER_EMAIL = "krishan15061998@gmail.com";
 
